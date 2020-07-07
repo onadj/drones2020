01c0f5fac2523df37f0db5dc29fe93141e72993e
onadj <oliver.nad@gmail.com>
2020-07-07 11:35:44 +0000

    adddrones.html

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'drones2020'
+app.config["MONGO_URI"] = 'mongodb+srv://onadj:Signacare2020@myfirstcluster-hbcie.mongodb.net/drones2020?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_drones')
+def get_drones():
+    return render_template("adddrones.html", drones=mongo.db.drones.find())
 
 
 if __name__ == '__main__':
