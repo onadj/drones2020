df8b05f339022d862c216a73b3708323602198bc
onadj <oliver.nad@gmail.com>
2020-07-07 17:59:03 +0000

    Push to Heroku

--- app.py
+++ app.py
@@ -13,10 +13,28 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_drones')
 def get_drones():
-    return render_template("adddrones.html", drones=mongo.db.drones.find())
+    return render_template("drones.html", drones=mongo.db.drones.find())
+
+@app.route('/add_drone')
+def add_drone():
+    return render_template('add_drone.html',categories=mongo.db.categories.find())
+
+@app.route('/insert_drone', methods=['POST'])
+def insert_drone():
+    drones = mongo.db.drones
+    drones.insert_one(request.form.to_dict())
+    return redirect(url_for('get_drones'))
+
+@app.route('/edit_drone/<drone_id>')
+def edit_drone(drone_id):
+    the_drone =  mongo.db.drones.find_one({"_id": ObjectId(drone_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('edit_drone.html', drone=the_drone,
+                           categories=all_categories)
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
+
